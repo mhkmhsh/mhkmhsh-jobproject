a6f622034fa594a3ec00f8a99df7ddacf4bcc25e
Mahek Mahesh <144465044+mhkmhsh@users.noreply.github.com>
2024-04-26 17:25:24 +0000

    set everything up just add content

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const text2 = document.getElementById('para2')
 const text3 = document.getElementById('para3')
 const image1 = document.getElementById('img1')
 const image2 = document.getElementById('img2')
+
+
+function general() {
+    title.innerHTML=''
+    image1.src= ``
+    image2.src= ``
+    text1.innerHTML=''
+    text2.innerHTML=''
+    text3.innerHTML=''
+}
+function journey() {
+    title.innerHTML=''
+    image1.src= ``
+    image2.src= ``
+    text1.innerHTML=''
+    text2.innerHTML=''
+    text3.innerHTML=''
+}
+function myself() {
+    title.innerHTML=''
+    image1.src= ``
+    image2.src= ``
+    text1.innerHTML=''
+    text2.innerHTML=''
+    text3.innerHTML=''
+}
