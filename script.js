b1b3c8a442129471c1769c4ee23eb4d04808dbd9
mhkmhsh <mahekmahesh12@gmail.com>
2024-04-29 09:43:09 -0400

    idk rn

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const image2 = document.getElementById('img2')
 
 
 function general() {
-    title.innerHTML=''
-    image1.src= `image/`
-    image2.src= `image/`
-    text1.innerHTML=''
-    text2.innerHTML=''
-    text3.innerHTML=''
+    title.innerHTML=`The Facts`
+    image1.src= 'image/'
+    image2.src= 'image/'
+    text1.innerHTML=`Being a Formula One software engineer means working at the cutting edge of technology, where milliseconds can make all the difference. It involves developing complex algorithms and software solutions to optimize car performance, analyzing massive amounts of data in real-time, and collaborating closely with engineers, data scientists, and drivers to push the boundaries of speed and efficiency. It's a thrilling blend of innovation, precision, and high-pressure teamwork in the fast-paced world of motorsport.`
+    text2.innerHTML=``
+    text3.innerHTML=``
 }
 function journey() {
     title.innerHTML=''
